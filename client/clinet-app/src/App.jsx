50d5a18a616f8b782814b2dd729998f591fd6a20
thagana <samuelmthwa79@gmail.com>
2019-10-14 02:44:27 -0700

    root component

--- client/clinet-app/src/App.jsx
+++ client/clinet-app/src/App.jsx
@@ -2,17 +2,23 @@ import React from 'react';
 import Landing from './components/Landing';
 import Login from './components/Login';
 import Register from './components/Register';
-import Profile from './components/Profile';
-import { HashRouter as Router, Route} from 'react-router-dom';
+import { Authentication } from './components/functions/authentication';
+import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route  {...rest} render={(props) => (
+      Authentication.isAuthenticated === true ? <Component { ...props} /> : <Redirect to='/login' />
+    )} />
+  )
 
 function App() {
   return (
       <Router>
       <div className="App">
-        <Route exact path="/" component={Landing} />
+        <PrivateRoute  path="/" component={Landing} />
         <div className="container">
             <Route exact path="/login" component={Login} />
-            <Route exact path="/profile" component={Profile} />
             <Route exact path="/register" component={Register} />
         </div>
       </div>
