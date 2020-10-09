88bbd170de967d171c3cb073cd32b950cc4bbbba
Thagana <samuelmthwa79@gmail.com>
2020-10-09 11:58:06 +0200

    dependency

--- flashcards/src/Root.js
+++ flashcards/src/Root.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStore, StoreProvider as Provider } from 'easy-peasy';
+import Store from './store/model';
+
 import Routes from './routes/Routes';
+
+const store = createStore(Store);
 
 export default function RootConatiner(){
     return (
-        <NavigationContainer>
-           <Routes />
-        </NavigationContainer>
-    )
+        <Provider store={store}>
+            <NavigationContainer>
+                <Routes />
+            </NavigationContainer>
+        </Provider>
+        )
 }
