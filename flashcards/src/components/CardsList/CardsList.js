5bf77bdc167760398b532f38d302460a20d434de
Thagana <samuelmthwa79@gmail.com>
2020-10-11 14:48:26 +0200

    component update

--- flashcards/src/components/CardsList/CardsList.js
+++ flashcards/src/components/CardsList/CardsList.js
@@ -1,20 +1,46 @@
 import React from 'react'
-import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity, Alert } from 'react-native'
+import { useStoreActions, useStoreState } from 'easy-peasy';
 import styles from './CardsList.style';
 
 export default function CardsList({props, cards}) {
-
-    function trim(text){
+    const value = useStoreState((state) => state.cards);
+    const deleteCard = useStoreActions((action) => action.deleteCard);
+    const trim = (text) => {
         if(text.length > 20){
             return text.substr(0, 20 - 1) + ' ...';
         }
         return text;
     }
-
+    const handleDelete = (id) => {
+        const data = value;
+        data.forEach((card, index) => {
+            if(card.cartigory === id){
+                data.splice(index, 1);
+            }
+        });
+        deleteCard(data);
+    }
+    const openDeleteDialog = (id) => {
+        Alert.alert(
+            "Delete Confirmation",
+            "Are you sur you want to delete the card?",
+            [
+              {
+                text: "Cancel",
+                onPress: () => console.log("Cancel Pressed"),
+                style: "cancel"
+              },
+              { text: "OK", onPress: () => handleDelete(id) }
+            ],
+            { cancelable: false }
+          );
+               
+    }
     function Card({ item }) {
         const { imageSrc, title, cartigory, content } = item;
         return (
-            <TouchableOpacity style={styles.cardContainer} onPress={() => {  props.navigation.navigate('Cards', { cards: content })  }} activeOpacity={0.8} >
+            <TouchableOpacity style={styles.cardContainer} onPress={() => {  props.navigation.navigate('Cards', { cards: content })  }} activeOpacity={0.8} onLongPress={() => openDeleteDialog(cartigory)}>
                 <View style={styles.imageConatiner}>
                     {
                         imageSrc ? (
--- flashcards/src/components/HeaderLeft/HeaderLeft.js
+++ flashcards/src/components/HeaderLeft/HeaderLeft.js
@@ -1,12 +1,52 @@
+import { useStoreActions } from 'easy-peasy';
 import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity, View, Text } from 'react-native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import Modal from 'react-native-modal';
 import styles from './HeaderLeft.style';
 
 export default function HeaderLeft() {
+    const [open, setOpen] = React.useState(false);
+    const openModal = () => {
+        setOpen(!open)
+    }
+    const handleDeleteAll = () => {
+
+    }
     return (
-        <TouchableOpacity style={styles.container} onPress={() => {}}>
+        <TouchableOpacity style={styles.container} onPress={openModal} coverScreen={true}>
             <Ionicons name="md-options" size={27} color="#000" />
+            <Modal isVisible={open} style={styles.modal}>
+                <View style={styles.modalContainer}>
+                    <View>
+                        <TouchableOpacity onPress={handleDeleteAll} style={styles.options}>
+                            <View>
+                                <Text style={styles.deleteText}>
+                                    Delete All
+                                </Text>
+                            </View>
+                            <View>
+                                <Ionicons name="md-trash-outline" size={26} color="red" />
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.options}>
+                            <View>
+                                <Text style={styles.mode}>
+                                    Dark Mode
+                                </Text>
+                            </View>
+                            <View>
+                                <Ionicons name="md-sunny-sharp" color="orange" size={26} />
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                    <View>
+                        <TouchableOpacity onPress={openModal} style={styles.closeBtnContainer}>
+                            <Text style={styles.closeBtn}>Close</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
         </TouchableOpacity>
     )
 }
