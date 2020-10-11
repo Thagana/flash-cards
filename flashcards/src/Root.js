62882dff2c11d744f6a29a036c25fb9b204baaac
Thagana <samuelmthwa79@gmail.com>
2020-10-11 14:49:02 +0200

    Root component for store persist

--- flashcards/src/Root.js
+++ flashcards/src/Root.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStore, StoreProvider as Provider } from 'easy-peasy';
+import { createStore, StoreProvider as Provider, persist } from 'easy-peasy';
+import storage from './store/storage/storage';
+
 import Store from './store/model';
 
 import Routes from './routes/Routes';
 
-const store = createStore(Store);
+const store = createStore(
+    persist(Store, {
+        storage: storage,
+      }),
+);
 
 export default function RootConatiner(){
     return (
